5a5e814c8f3471b1b7da915a1243e59a46dbd62b
Samarpita Das <samarpitabdas@gmail.com>
2024-05-13 08:41:25 +0530

    login_activity

---
+++ app/src/main/java/com/example/raksha/LoginTabFragment.java
@@ -0,0 +1,17 @@
+package com.example.raksha;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class LoginTabFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstances){
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.login_tab_fragment, container, false);
+        return root;
+    }
+}
---
+++ app/src/main/java/com/example/raksha/SignUpTabFragment.java
@@ -0,0 +1,18 @@
+package com.example.raksha;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class SignUpTabFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstances){
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.sign_up_tab_fragment, container, false);
+        return root;
+    }
+
+}
