b4f973392b7a5869802f5a255e3b03c4988a1ba9
Samarpita Das <samarpitabdas@gmail.com>
2024-05-15 01:31:10 +0530

    login_activity

--- app/src/main/java/com/example/raksha/MainActivity.java
+++ app/src/main/java/com/example/raksha/MainActivity.java
@@ -20,26 +20,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Create an array adapter using the string array "role"
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.dropdown_item,roles);
-
-        // Inflate the layout containing AutoCompleteTextView
-        View signUpTabFragment = getLayoutInflater().inflate(R.layout.sign_up_tab_fragment, null);
-
-        // Get reference to the AutoCompleteTextView within the inflated layout
-        AutoCompleteTextView autoCompleteTextView = signUpTabFragment.findViewById(R.id.autoCompleteTextView);
-
-        // Set adapter to the autoCompleteTextView and specify the dropdown item layout
-        autoCompleteTextView.setAdapter(arrayAdapter);
-        autoCompleteTextView.setDropDownBackgroundResource(R.color.white); // Optional: Set background color of dropdown
-
-        // Ensure the dropdown list appears below the AutoCompleteTextView
-        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String item = adapterView.getItemAtPosition(i).toString();
-                Toast.makeText(MainActivity.this,"role" + roles,Toast.LENGTH_SHORT).show();
-            }
-        });
+//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.dropdown_item,roles);
+//
+//        // Inflate the layout containing AutoCompleteTextView
+//        View signUpTabFragment = getLayoutInflater().inflate(R.layout.sign_up_tab_fragment, null);
+//
+//        // Get reference to the AutoCompleteTextView within the inflated layout
+//        AutoCompleteTextView autoCompleteTextView = signUpTabFragment.findViewById(R.id.autoCompleteTextView);
+//
+//        // Set adapter to the autoCompleteTextView and specify the dropdown item layout
+//        autoCompleteTextView.setAdapter(arrayAdapter);
+//        autoCompleteTextView.setDropDownBackgroundResource(R.color.white); // Optional: Set background color of dropdown
+//
+//        // Ensure the dropdown list appears below the AutoCompleteTextView
+//        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                String item = adapterView.getItemAtPosition(i).toString();
+//                Toast.makeText(MainActivity.this,"role" + roles,Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 
 
