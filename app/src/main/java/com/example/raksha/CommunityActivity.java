a24bbc2416de79d786b312eaa7e888dc379bd349
Samarpita Das <samarpitabdas@gmail.com>
2024-05-17 23:57:24 +0530

    communityActivity_static

--- app/src/main/java/com/example/raksha/CommunityActivity.java
+++ app/src/main/java/com/example/raksha/CommunityActivity.java
@@ -1,5 +1,7 @@
 package com.example.raksha;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,8 +10,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.Firebase;
 import com.google.firebase.database.DataSnapshot;
@@ -23,23 +27,26 @@ public class CommunityActivity extends AppCompatActivity {
     //    TODO create adapters etc for community tab  to handle posting andd storing of the posts
     public String communityusername;
     private TextView usernameTextView, timestampTextView, postContentTextView;
+    private ImageView postphoto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community);
 
-        //initializing textview
+        // Initializing textview
         usernameTextView = findViewById(R.id.usernameTextView);
         timestampTextView = findViewById(R.id.timestampTextView);
         postContentTextView = findViewById(R.id.postContentTextView);
+        // Initializing ImageView
+        postphoto = findViewById(R.id.postimage);
 
-        //retriving username from intent
+        // Retrieving username from intent
         Intent intent = getIntent();
         communityusername = intent.getStringExtra("username");
 
-        //fetching users post content data from database
+        // Fetching user's post content data from database
         fetchUserContentFromDatabase(communityusername);
-
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -49,7 +56,7 @@ public class CommunityActivity extends AppCompatActivity {
                 Intent intent = getIntent();
 
                 communityusername = intent.getStringExtra("username");
-//                Log.d("LoginActivity", "Username: " + communityusername);
+
                 if (itemId == R.id.home){
                     Intent communityIntent = new Intent(CommunityActivity.this, HomeActivity.class);
                     communityIntent.putExtra("username", communityusername);
@@ -57,21 +64,16 @@ public class CommunityActivity extends AppCompatActivity {
                     startActivity(communityIntent);
                     return true;
                 } else if (itemId == R.id.emergency) {
-
-//                    Log.d("LoginActivity", "Username: " + communityusername);
                     startActivity(new Intent(CommunityActivity.this, EmergencyActivity.class)
                             .putExtra("username", communityusername));
                     return true;
                 } else if (itemId == R.id.report) {
-
-//                    Log.d("LoginActivity", "Username: " + communityusername);
                     startActivity(new Intent(CommunityActivity.this, ReportActivity.class)
                             .putExtra("username", communityusername));
                     return true;
-                }else {
+                } else {
                     return false;
                 }
-
             }
         });
     }
@@ -84,12 +86,25 @@ public class CommunityActivity extends AppCompatActivity {
                         for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
                             String username = postSnapshot.child("username").getValue(String.class);
                             String description = postSnapshot.child("description").getValue(String.class);
-                            String timerstamp = postSnapshot.child("timestamp").getValue(String.class);
+                            String timestamp = postSnapshot.child("timestamp").getValue(String.class);
+                            String imageUrl = postSnapshot.child("photo").getValue(String.class);
 
                             if (username != null && description != null) {
+                                // Display post details
                                 usernameTextView.append(username);
                                 postContentTextView.append(description + "\n\n");
-                                timestampTextView.append(timerstamp );
+                                timestampTextView.append(timestamp);
+
+                                Log.d(TAG, "Image URL: " + imageUrl);
+
+
+                                // Load image using Glide
+                                if (imageUrl != null && !imageUrl.isEmpty()) {
+
+                                    Glide.with(getApplicationContext())
+                                            .load(imageUrl)
+                                            .into(postphoto);
+                                }
                             }
                         }
                     }
@@ -101,5 +116,4 @@ public class CommunityActivity extends AppCompatActivity {
                     }
                 });
     }
-
 }
